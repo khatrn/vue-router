451fb926c316dad438d8b12515dec5fd7d7ac11c
Kha Tran <kha.tn@neo-lab.vn>
2021-03-11 10:14:51 +0700

    VR-7 Name View

--- vue-router/src/router/index.js
+++ vue-router/src/router/index.js
@@ -3,20 +3,40 @@ import Router from 'vue-router'
 import UserProfile from '../components/UserProfile'
 import UserPost from '../components/UserPost'
 import UserHome from '../components/UserHome'
+
 Vue.use(Router)
 
 const Foo = {template: '<div>Foo</div>'}
 const Bar = {template: '<div>Bar</div>'}
+const Baz = {template: '<div>Baz</div>'}
 const User = {
   template: `<div class="user">
         <h2>User {{ $route.params.id }}</h2>
         <router-view></router-view>
-        </div>` }
+        </div>`
+}
 
 const routes = [
-  { path: '/foo', name: 'Foo', component: Foo },
-  { path: '/bar', name: 'Bar', component: Bar },
-  { path: '/user/:id',
+  {
+    path: '/',
+    components: {
+      default: Foo,
+      a: Bar,
+      b: Baz
+    }
+  },
+  {
+    path: '/other',
+    components: {
+      default: Baz,
+      a: Bar,
+      b: Foo
+    }
+  },
+  {path: '/foo', name: 'Foo', component: Foo},
+  {path: '/bar', name: 'Bar', component: Bar},
+  {
+    path: '/user/:id',
     component: User,
     children: [
       {
